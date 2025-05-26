eb9c1be7eec291e21847c5bcf617dd25f4c9fd83
Dhaval Harkhani <dhaval.harkhani@sourceinfinity.com>
2025-05-26 14:40:24 +0530

    Update day_2.py

--- day_2.py
+++ day_2.py
@@ -1,3 +1,5 @@
+#calculator using arithmmatic operators
+
 num1 = int(input("Enter first number: "))
 num2 = int(input("enter second number: "))
 
@@ -6,3 +8,15 @@ print("difference:", num1 - num2)
 print("product:", num1 * num2)
 print("division:", num1 / num2)
 print("modulus:", num1 % num2)
+
+#datatype revealed by type() function
+
+a = "dhaval"
+b = 5
+c = 5.2
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
