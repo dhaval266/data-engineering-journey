34b786b35c1e8410143b4f4615156ae10bfa812f
Dhaval Patel <79134634+dhaval266@users.noreply.github.com>
2025-05-26 13:23:51 +0530

    day_2_calculator.py

---
+++ day_2.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("enter second number: "))
+
+print("sum:", num1 + num2)
+print("difference:", num1 - num2)
+print("product:", num1 * num2)
+print("division:", num1 / num2)
+print("modulus:", num1 % num2)
